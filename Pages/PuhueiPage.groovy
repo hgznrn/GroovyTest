1572d224b0829d0f56f8ccce95e55eedae9046b0
MarkShine <hgznrn@gmail.com>
2016-12-09 18:26:44 +0800

    case1 version0.1

--- Pages/PuhueiPage.groovy
+++ Pages/PuhueiPage.groovy
@@ -1,14 +1,28 @@
 package Pages
 
 class PuhueiPage extends geb.Page {
+    
     static url = "http://www.puhuei.com/login"
+
     static at = { waitFor{ title == '會員登入'} }
+
+    
+    //會員密碼
     static content = {
-        usernameInput { $("input[name='username']") } //會員帳號
-        passwordInput { $("input[name='password']") } //會員密碼
+
+        //會員帳號輸入框
+        usernameInput { $("input[name='username']") } 
+
+        //會員密碼輸入框
+        passwordInput { $("input[name='password']") } 
+
+        //會員登入
         btnLogin {$("button[id='login-button']") }
+
     }
-    void login(String account, String password) { //登入
+
+    //登入
+    void login(String account, String password) { 
         usernameInput.value(account)
         passwordInput.value(password)
         btnLogin.click()
--- TestCase/case1.groovy
+++ TestCase/case1.groovy
@@ -5,9 +5,16 @@ import au.com.bytecode.opencsv.*
 import groovy.util.*
 import Pages.PuhueiPage
 
-RunScript = {
-	geb.Browser.drive {
-		println "drive"
+RunScript = 
+{
+	geb.Browser.drive 
+	{
+		if(this.args.size() != 4) //包含
+	    {
+	        println "Warring!! Args is 3 number(Year Month Day)"
+	        return false
+	    }
+
 	    to PuhueiPage
 	    at PuhueiPage
 	    login("system", "coke08150114")
@@ -15,14 +22,16 @@ RunScript = {
 	}.quit()
 }
 
-RunMainScript = { 
-	geb.Browser.drive {
-		if(this.args.size() != 3)
-	    {
-	        println "Warring!! Args is 3 number(Year Month Day)"
-	        return false
-	    }
+RunMainScript = 
+{ 
+	def flag = false;
+	geb.Browser.drive 
+	{
+		//設定限制3個參數
+		flag = CheckArgsNum(3)
 	}
+	if (flag) return geb.Browser.drive
+	else return null
 }
 
 
